5f257406ebf5008c6e11cc8ae8d5269fdf9bee75
Haley Jacobs <haleyljacobs@gmail.com>
2018-05-10 12:33:35 -0600

    Added beginning of tests

---
+++ test/mergeSort-test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Car = require('../lib/mergeSort.js');
+
+//test to make sure that sorted only has one value
+//test to make sure that the order of the array is in increasing value
